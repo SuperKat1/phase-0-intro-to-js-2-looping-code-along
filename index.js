29592e42d1aac86ad71244ff8295f6c394b32999
Sandra Katsikis <mindfulripples@gmail.com>
2023-07-21 21:49:55 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const experience = "birthday";
+
+
+function writeCards(names, experience) {
+    const message = [];
+    for (let i = 0; i < names.length; i++) {
+        message[i] = (`Thank you, ${names[i]}, for the wonderful ${experience} gift!`);
+  }
+
+  return message;
+}
+
+console.log(writeCards(names, experience)); 
+
+
+let counter = 12;
+function countDown(counter) {
+    while (counter >= 0) {
+        console.log(counter);
+        counter--;
+    }
+    return counter;
+}
+
+countDown(counter);
+
